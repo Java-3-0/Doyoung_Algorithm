ef9f43da4dce4a8b10e265bf3b534a9a6e21caab
DoyoungYim <98091936+DoyoungYim@users.noreply.github.com>
2022-02-10 23:45:15 +0900

    Add files via upload

---
+++ src/2022/02/week3/2605_줄_세우기.java
@@ -0,0 +1,36 @@
+// 17636KB, 220ms
+
+package baek2605;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numStudents = sc.nextInt();
+		List<Integer> studentLine = new ArrayList<>();
+		
+		// 줄 세우기
+		for (int num = 1; num <= numStudents; num++) {
+			// 앞으로 갈 칸 수 입력
+			int moveForward = sc.nextInt();
+			// 도착하는 칸의 인덱스 계산
+			int idx = num - 1 - moveForward;
+			// 그 칸에 배치
+			studentLine.add(idx, num);
+		}
+		
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		for (int num : studentLine) {
+			sb.append(num).append(" ");
+		}
+		sb.append("\n");
+		System.out.print(sb);
+	}
+
+}
