ae915058ed0a81718df8e8b1eb4fcbcb8c460f71
DoyoungYim <98091936+DoyoungYim@users.noreply.github.com>
2022-02-12 00:02:11 +0900

    Add files via upload

---
+++ src/2022/02/week2/백준_15657_N과_M_8.java
@@ -0,0 +1,56 @@
+// 20892KB, 276ms
+
+package baek15657;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static int N, M;
+	public static int[] input;
+	public static int[] combi;
+	public static StringBuilder sb;
+
+	public static void main(String[] args) {
+		// N, M 입력
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		// 전역변수 레퍼런스 할당
+		combi = new int[M];
+		sb = new StringBuilder();
+		input = new int[N];
+		
+		// 수열 입력
+		for (int i = 0; i < N; i++) {
+			input[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		// 입력받은 수열 오름차순 정렬
+		Arrays.sort(input);
+
+		// 중복조합 생성
+		combination(0, 0);
+
+		// 출력
+		System.out.print(sb.toString());
+	}
+
+	/** 중복조합을 계산해서 스트링빌더에 담는다 */
+	public static void combination(int startIdx, int count) {
+		if (count == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(combi[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int pickIdx = startIdx; pickIdx < N; pickIdx++) {
+			combi[count] = input[pickIdx];
+			combination(pickIdx, count + 1);
+		}
+	}
+}
