0682f2f2bc36937846fcebf67fd9d203aca1bbc2
DoyoungYim <98091936+DoyoungYim@users.noreply.github.com>
2022-02-11 09:19:02 +0900

    Add files via upload

---
+++ src/2022/02/week2/백준_2559_수열.java
@@ -0,0 +1,52 @@
+// 24276KB, 296ms
+
+package baek2559;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int seq[] = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 부분합 계산
+		int psums[] = getPsums(seq);
+
+		// 구간합의 최대 계산
+		int answer = Integer.MIN_VALUE;
+		for (int i = K; i < psums.length; i++) {
+			answer = Math.max(answer, psums[i] - psums[i - K]);
+		}
+
+		System.out.println(answer);
+	}
+
+	/** 부분합을 계산해서 배열로 리턴 */
+	public static int[] getPsums(int[] arr) {
+		int[] ret = new int[arr.length];
+
+		ret[0] = 0;
+		int sum = 0;
+		for (int i = 1; i < arr.length; i++) {
+			sum += arr[i];
+			ret[i] = sum;
+		}
+
+		return ret;
+	}
+
+}
