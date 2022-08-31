2356b72c5defaabe45d950b1bedae2789a467af0
Doyoung Yim <dyim0403@gmail.com>
2022-08-31 17:37:10 +0900

    20220831

---
+++ src/2022/08/week5/백준_1461_도서관.java
@@ -0,0 +1,66 @@
+// 11864KB, 84ms
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final int INF = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> leftPQ = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> rightPQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 입력을 받아서 방향마다 절대값을 리스트에 담는다.
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num >= 0) {
+                rightPQ.offer(num);
+            } else {
+                leftPQ.offer(-num);
+            }
+        }
+
+        int answer = 0;
+
+        // 마지막에 방문할 곳 처리
+        int maxLeft = leftPQ.isEmpty() ? 0 : leftPQ.peek();
+        int maxRight = rightPQ.isEmpty() ? 0 : rightPQ.peek();
+        answer -= Math.max(maxLeft, maxRight);
+
+        while (!leftPQ.isEmpty()) {
+            for (int i = 0; i < M; i++) {
+                if (!leftPQ.isEmpty()) {
+                    int distance = leftPQ.poll();
+                    if (i == 0) {
+                        answer += (2 * distance);
+                    }
+                }
+            }
+        }
+
+        while (!rightPQ.isEmpty()) {
+            for (int i = 0; i < M; i++) {
+                if (!rightPQ.isEmpty()) {
+                    int distance = rightPQ.poll();
+                    if (i == 0) {
+                        answer += (2 * distance);
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
